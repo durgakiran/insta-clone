c36b2e763b6cd6792cb155b8b13c554bd00ee5f7
durgakiran <gatemefuture@gmail.com>
2021-04-10 10:54:05 +0530

    Add secret to kuberctl infra

--- ticketing/Auth/src/index.ts
+++ ticketing/Auth/src/index.ts
@@ -1,47 +1,6 @@
-import express from 'express';
-import { json } from 'body-parser';
-import { currentUserRouter } from './routes/current-user';
-import { signInRouter } from './routes/signin';
-import { signOutRouter } from './routes/singout';
-import { signUpRouter } from './routes/signup';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
-import { clientDB } from './db';
-import { DatabaseConnectionError } from './errors/database-connection-error';
-import cors from "cors";
+import app from './app';
 
-const app = express();
-app.use(json());
-app.use(cors());
-app.use(
-    currentUserRouter, 
-    signInRouter, 
-    signOutRouter, 
-    signUpRouter
-);
-
-app.all('*', async (req, res, next) => {
-    try {
-        throw new NotFoundError(); 
-    } catch (err) {
-        next(err);
-    }
-});
-
-app.use(errorHandler);
-
-const start = async () => {
-    try {
-        await clientDB.connect();
-        await clientDB.query(`CREATE TABLE IF NOT EXISTS
-            users (email VARCHAR(200) PRIMARY KEY, password VARCHAR NOT NULL, name VARCHAR NOT NULL);`);
-        console.log('connection successful');
-    } catch (err) {
-        console.log(err);
-        throw new DatabaseConnectionError();
-    }
-
-    // start the server
+const start = async () => { // start the server
     app.listen(3000, () => {
         console.log('listening on port 3000!');
     });
--- ticketing/Auth/src/services/SignIn.service.ts
+++ ticketing/Auth/src/services/SignIn.service.ts
@@ -14,14 +14,12 @@ export default async function SingInService(req: Request, res: Response, next: N
             throw new RequestValidationError(errors.array());
         }
 
-        const { email, password } = req.body;
+        const {email, password } = req.body;
         const result = await getEmail(email);
-        
-
         try {
             if(await comparePasswords(result.password, password)) {
-                const token = jsonwebtoken.sign({email }, 'shhh');
-                res.send({ token: token, email, name: result.name });
+                const token = jsonwebtoken.sign({email }, process.env.JWT_KEY!);
+                res.send({ token: token, email, name: result.name, id: result.user_id });
             } else {
                 res.status(403).end();
             }
@@ -35,7 +33,7 @@ export default async function SingInService(req: Request, res: Response, next: N
     }
 }
 
-async function getEmail(email: string): Promise<{ email: string; password: string; name: string }> {
+async function getEmail(email: string): Promise<{ email: string; password: string; name: string; user_id: string }> {
     const [count, rows] = await findUserByEmail(email);
     if(count === 0) {
         throw new BadRequestError('User with email does not exists');
